a1526f453f8df9d2b3fefd6c79f8e1439a3df1cc
Alexey Ryazanov <solargatemusic@gmail.com>
2023-03-05 01:46:49 +0500

    chore: добавлен https для локальной разработки

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
+// https://github.com/sapphi-red/vite-setup-catalogue
 export default defineConfig({
   plugins: [react(), eslint()],
+  server: {
+    https: {
+      cert: path.join(__dirname, 'zabastcom.local+4.pem'),
+      key: path.join(__dirname, 'zabastcom.local+4-key.pem'),
+    },
+    host: true,
+    strictPort: true,
+  },
 })
