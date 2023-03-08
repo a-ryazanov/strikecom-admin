407c98861ce98869ce9292ede3b31864098c56d2
Alexey Ryazanov <solargatemusic@gmail.com>
2023-03-09 00:30:13 +0500

    fix: исправлен цвет активного элемента меню в сайдбаре

--- src/app/ui/sidebar-menu.tsx
+++ src/app/ui/sidebar-menu.tsx
@@ -1,6 +1,7 @@
 import { AlertOutlined, FireOutlined, MessageOutlined } from '@ant-design/icons'
 import { Link, useRouter } from '@tanstack/react-router'
 import { Menu } from 'antd'
+import cn from 'classnames'
 import React from 'react'
 
 import { CONFLICTS_ROUTE_PATH, EVENTS_ROUTE_PATH, NEWS_ROUTE_PATH } from '../../shared/config'
@@ -26,13 +27,22 @@ export const SidebarMenu: React.FC = () => {
   const { state } = useRouter()
   const activePathname = state.currentLocation.pathname
 
-  const items = routes.map((item) => ({
-    key: item.title,
-    pathname: `/${item.path}`,
-    // @ts-expect-error. Неясно, почему ошибка, документация сырая
-    label: <Link to={absolutePath(item.path)}>{item.title}</Link>,
-    icon: <item.icon />,
-  }))
+  const items = routes.map((item) => {
+    const pathname = absolutePath(item.path)
+    const isActive = pathname === activePathname
+
+    return {
+      key: item.title,
+      pathname,
+      label: (
+        // @ts-expect-error. Неясно, почему ошибка, документация сырая
+        <Link to={pathname} className={cn(isActive ? styles.menuItem__label_active : null)}>
+          {item.title}
+        </Link>
+      ),
+      icon: <item.icon className={cn(isActive ? styles.menuItem__icon_active : null)} />,
+    }
+  })
 
   const selectedKeys = items
     .filter((item) => item.pathname === activePathname)
